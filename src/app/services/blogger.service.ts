018e56f7342fbe35f475835dda4ef47b5d0770fa
Diego Nobre <diegonobre.dev@outlook.com>
2024-07-24 12:50:32 -0300

    ajuste key

--- src/app/services/blogger.service.ts
+++ src/app/services/blogger.service.ts
@@ -8,7 +8,7 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class BloggerService {
 
-  private apiKey = 'key=AIzaSyCLoF7T9NZKd3FM5qnoO8wmYgWXGezjosg'; // Chave de API do Google
+  private apiKey = 'AIzaSyCLoF7T9NZKd3FM5qnoO8wmYgWXGezjosg'; // Chave de API do Google
   private blogId = '7973829152381710727'; // ID do seu blog no Blogger
   private baseUrl = 'https://www.googleapis.com/blogger/v3/blogs';
 
